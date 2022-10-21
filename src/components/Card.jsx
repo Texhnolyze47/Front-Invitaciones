fe8c4e81700f7223ee8ebe4d0ffdfab7651a4890
josafat <josa221001@gmail.com>
2022-10-20 22:23:21 -0500

    add: cambiando sintaxis

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,8 @@ import '../styles/Card.css';
 
 function Testimonio(){
     return(
+
+        // <React.Fragment>
         <div className="card">
             
             <img
@@ -14,53 +16,15 @@ function Testimonio(){
             <h2 className='title'>ESCIHU-GEEKS</h2>
             <p className='text-info'>Ingresa tu usuario de Github sin @</p>
             <input
-                className='input' 
-                type="text" 
+                className='email' 
+                type="email" 
                 required
             />
-            <button className='send' type="submit" onClick={() => h()}>Ingresar</button>
-
+            <button className='send' type="submit" onClick={() => alert('Hola')}>Ingresar</button>
 
         </div>
+        // </React.Fragment>
     );
 }
-
-function h(){
-    const user = document.querySelector('.input').value;
-
-    const url = `https://api.github.com/users/${user}`;
-
-    fetch(url, { 
-                 headers: {
-                      'Accept' : 'application/vnd.github.v3+json'
-                  }})
-		.then(response => response.json()) //Converting the response to a JSON object
-		.then( data => {
-                    alert('Usuario encontrado');
-                    console.log(data);
-                })
-		.catch( error => {
-            console.error(error)
-            alert('Usuario no encontrado');
-        });
-
-
-
-
-    // fetch(url).then((res) => {
-    //     if (res.status !== "200") {
-    //         console.log(res);
-    //         alert('Usuario encontrado');
-    //     }
-    //     else {
-    //         return res.json();
-    //     }
-    // }).then((data) => {
-    //         console.log(data)
-    // })
-
-}
-
-
 
 export default Testimonio;
